2a7794838ab87a2d1132a239fbf6f283de0b3ccf
naitikbajpaii <167636350+naitikbajpaii@users.noreply.github.com>
2024-04-26 18:44:27 +0530

    Create Ques2.ino

---
+++ Exercise 1/Ques2.ino
@@ -0,0 +1,22 @@
+int LED1=13;	
+int LED2=12;	
+int LED3=11;
+
+void setup() {	
+ pinMode(LED1,OUTPUT);
+ pinMode(LED2,OUTPUT);
+ pinMode(LED3,OUTPUT);
+}
+
+	
+void loop() {
+digitalWrite(LED1,HIGH);
+delay(1000);
+digitalWrite(LED1,LOW);
+digitalWrite(LED2,HIGH);
+delay(1000);
+digitalWrite(LED2,LOW);
+digitalWrite(LED3,HIGH);
+delay(1000);
+digitalWrite(LED3,LOW);
+}
