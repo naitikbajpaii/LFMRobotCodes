e0858d6adcdc6fe128f3ddca6caaa7173afbd09c
Naitik Bajpai <167636350+naitikbajpaii@users.noreply.github.com>
2024-05-06 09:16:29 +0530

    Create Ques9.ino

---
+++ Exercise 1/Ques9.ino
@@ -0,0 +1,8 @@
+void setup() {
+  Serial.begin(9600);  // Setup of serial communication module
+}
+
+void loop() {
+  Serial.println("Group20, Serial Monitor text"); // showing some text in the serial monitor
+  delay(1000);
+}
