f8c29ec8ff79793688b41cc89410f1ec3250a304
Naitik Bajpai <167636350+naitikbajpaii@users.noreply.github.com>
2024-05-06 11:51:29 +0530

    Update Ques10.ino

--- Exercise 1/Ques10.ino
+++ Exercise 1/Ques10.ino
@@ -3,8 +3,8 @@ int outputValue = 0;
 
 void setup()
 {
-  pinMode(A0, INPUT);
-  pinMode(9, OUTPUT);
+  pinMode(A2, INPUT);
+  pinMode(9, OUTPUT);//LED
   Serial.begin(9600);
 }
 
@@ -12,7 +12,7 @@ void loop()
 {
   value = analogRead(A2);
   outputValue = map(value, 0, 1023, 0, 255);
-  analogWrite(9, outputValue);
+  analogWrite(9, outputValue);//Change LED according to potentiometer
   Serial.println(outputValue);
   delay(2); 
 }
